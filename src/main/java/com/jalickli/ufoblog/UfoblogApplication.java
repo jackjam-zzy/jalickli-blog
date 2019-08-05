31b539046b0650b1d5f311748159f34c76f08c7f
jack_glb <jack_glb@jalickli.com>
2019-08-05 19:52:19 +0800

    first commit

---
+++ src/main/java/com/jalickli/ufoblog/UfoblogApplication.java
@@ -0,0 +1,13 @@
+package com.jalickli.ufoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfoblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfoblogApplication.class, args);
+	}
+
+}
